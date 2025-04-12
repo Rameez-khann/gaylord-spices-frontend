b5da444c5105f0bbbdd48323ef35d6e69caf7a93
Rameez Khann <victor@MacBook-Air.local>
2025-04-12 14:22:38 +0300

    Added Javascript Code for various functions

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function getFoods(){
-    // let foods = 
-    // return foods
-}
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Search input
+    const searchInput = document.getElementById('search');
+
+    //  Search Button 
+    const searchBtn = document.getElementById('search-btn');
+
+    //  Search Results
+    const resultsContainer = document.getElementById('results');
+
+    //  Loading
+    const loadingElement = document.getElementById('loading');
+
+    // Error message if recipes not found
+    const errorElement = document.getElementById('error');
+
+
+})
